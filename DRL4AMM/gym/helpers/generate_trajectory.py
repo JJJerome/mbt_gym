dbea74f86fb53860d200e142087f6963131ef6be
joe <JosephJerome94@gmail.com>
2022-08-25 19:14:03 +0100

    name action dim

--- DRL4AMM/gym/helpers/generate_trajectory.py
+++ DRL4AMM/gym/helpers/generate_trajectory.py
@@ -8,15 +8,16 @@ from DRL4AMM.agents.Agent import Agent
 def generate_trajectory(env: gym.Env, agent: Agent, seed: int = None, include_log_probs: bool = False):
     if seed is not None:
         np.random.seed(seed)
-    observations = np.zeros((env.num_trajectories, env.observation_space.shape[0], env.n_steps + 1))
-    actions = np.zeros((env.num_trajectories, env.action_space.shape[0], env.n_steps))
+    obs_space_dim = env.observation_space.shape[0]
+    action_space_dim = env.action_space.shape[0]
+    observations = np.zeros((env.num_trajectories, obs_space_dim, env.n_steps + 1))
+    actions = np.zeros((env.num_trajectories, action_space_dim, env.n_steps))
     rewards = np.zeros((env.num_trajectories, 1, env.n_steps))
     if include_log_probs:
         log_probs = torch.zeros((env.num_trajectories, env.action_space.shape[0], env.n_steps))
     obs = env.reset()
     observations[:, :, 0] = obs
     count = 0
-
     while True:
         if include_log_probs:
             action, log_prob = agent.get_action(obs, include_log_probs=True)

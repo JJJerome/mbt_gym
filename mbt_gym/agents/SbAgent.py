42ac82d63e0e196e8eab98731954a11d63eb115e
joe <JosephJerome94@gmail.com>
2022-11-21 10:53:44 +0000

    Specify reduced training indices in SbAgent

--- mbt_gym/agents/SbAgent.py
+++ mbt_gym/agents/SbAgent.py
@@ -6,13 +6,17 @@ from stable_baselines3.common.base_class import BaseAlgorithm
 
 
 class SbAgent(Agent):
-    def __init__(self, model: BaseAlgorithm, reduced_training: bool = False):
+    def __init__(self, model: BaseAlgorithm, reduced_training_indices: list = None):
         self.model = model
-        self.reduced_training = reduced_training
+        if reduced_training_indices is not None:
+            self.reduced_training = True
+            self.reduced_training_indices = reduced_training_indices
+        else:
+            self.reduced_training = False
 
     def get_action(self, state: np.ndarray) -> np.ndarray:
         if self.reduced_training:
-            state = state[-2:]
+            state = state[:, self.reduced_training_indices]
         return np.reshape(self.model.predict(state, deterministic=True)[0], -1)
 
     def train(self, total_timesteps: int = 100000):

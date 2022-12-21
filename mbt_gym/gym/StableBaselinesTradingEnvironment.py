727221181a98066496f89b9425429fe6ddfc6e92
Joe <JosephJerome94@gmail.com>
2022-12-21 15:45:27 +0000

    Add action and observation normalisation for StableBaselinesTradingEnvironment.py

--- mbt_gym/gym/StableBaselinesTradingEnvironment.py
+++ mbt_gym/gym/StableBaselinesTradingEnvironment.py
@@ -1,4 +1,5 @@
-from typing import List, Any, Type, Optional, Union
+from copy import deepcopy
+from typing import List, Any, Type, Optional, Union, Callable, Sequence
 
 import gym
 import numpy as np
@@ -9,20 +10,64 @@ from mbt_gym.gym.TradingEnvironment import TradingEnvironment
 
 
 class StableBaselinesTradingEnvironment(VecEnv):
-    def __init__(self, trading_env: TradingEnvironment, store_terminal_observation_info: bool = True):
+    def __init__(
+        self,
+        trading_env: TradingEnvironment,
+        store_terminal_observation_info: bool = True,
+        normalise_action_space: bool = True,
+        normalise_observation_space: bool = True,
+    ):
         self.env = trading_env
         self.store_terminal_observation_info = store_terminal_observation_info
+        self.normalise_action_space = normalise_action_space
+        self.normalise_observation_space = normalise_observation_space
         self.actions: np.ndarray = self.env.action_space.sample()
-        super().__init__(self.env.num_trajectories, self.env.observation_space, self.env.action_space)
+        if self.normalise_action_space:
+            # We just do a linear normalisation of the gym.Box space so that the domain of the action space is [-1,1].
+            self.linear_intercept_action = self.env.action_space.low
+            self.linear_gradient_action = (self.env.action_space.high - self.env.action_space.low) / 2
+            self.normalise_action: Callable = (
+                lambda action: (action - self.linear_intercept_action) / self.linear_gradient_action - 1
+            )
+            self.denormalise_action: Callable = (
+                lambda action: (action + 1) * self.linear_gradient_action + self.linear_intercept_action
+            )
+            action_space = gym.spaces.Box(
+                low=-np.ones_like(self.env.action_space.low), high=np.ones_like(self.env.action_space.high)
+            )
+        else:
+            action_space = self.env.action_space
+        if self.normalise_observation_space:
+            # Linear normalisation of the gym.Box space so that the domain of the observation space is [-1,1].
+            self.linear_intercept_observation = self.env.observation_space.low
+            self.linear_gradient_observation = (self.env.observation_space.high - self.env.observation_space.low) / 2
+            self.normalise_observation: Callable = (
+                lambda observation: (observation - self.linear_intercept_observation) / self.linear_gradient_observation
+                - 1
+            )
+            self.denormalise_observation: Callable = (
+                lambda observation: (observation + 1) * self.linear_gradient_observation
+                + self.linear_intercept_observation
+            )
+            observation_space = gym.spaces.Box(
+                low=-np.ones_like(self.env.observation_space.low), high=np.ones_like(self.env.observation_space.high)
+            )
+        else:
+            observation_space = gym.spaces.Box(
+                low=-np.ones_like(self.env.observation_space.low), high=np.ones_like(self.env.observation_space.high)
+            )
+        super().__init__(self.env.num_trajectories, observation_space, action_space)
 
     def reset(self) -> VecEnvObs:
         return self.env.reset()
 
     def step_async(self, actions: np.ndarray) -> None:
-        self.actions = actions
+        self.actions = self.denormalise_action(actions) if self.normalise_action_space else actions
 
     def step_wait(self) -> VecEnvStepReturn:
         state, rewards, dones, infos = self.env.step(self.actions)
+        if self.normalise_observation_space:
+            state = self.normalise_observation(state)
         if dones.min():
             if self.store_terminal_observation_info:
                 for info, count in enumerate(infos):
@@ -47,4 +92,7 @@ class StableBaselinesTradingEnvironment(VecEnv):
         return [False for _ in range(self.env.num_trajectories)]
 
     def seed(self, seed: Optional[int] = None) -> List[Union[None, int]]:
-        self.env.seed(seed)
+        return self.env.seed(seed)
+
+    def get_images(self) -> Sequence[np.ndarray]:
+        pass

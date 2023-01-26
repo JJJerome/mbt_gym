cee84f3c82d14bdbfcee893e219ac0f61fe39d2d
joe <JosephJerome94@gmail.com>
2023-01-26 11:58:50 +0000

    Only overwrite required functions

--- mbt_gym/gym/MultiprocessTradingEnv.py
+++ mbt_gym/gym/MultiprocessTradingEnv.py
@@ -14,6 +14,8 @@ from stable_baselines3.common.vec_env.base_vec_env import (
     VecEnvStepReturn,
 )
 
+from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv, _flatten_obs
+
 STORE_TERMINAL_OBSERVATION_INFO = True
 
 
@@ -67,7 +69,7 @@ def _worker(
             break
 
 
-class MultiprocessTradingEnv(VecEnv):
+class MultiprocessTradingEnv(SubprocVecEnv):
     """
     This is a slight modification of SubprocVecEnv, the details of which can be found at
     https://stable-baselines3.readthedocs.io/en/master/guide/vec_envs.html#subprocvecenv.
@@ -75,55 +77,10 @@ class MultiprocessTradingEnv(VecEnv):
     In particular, it modifies it SubprocVecEnv that the inputs are already VecEnvs. This allows the user to choose the
     amount of vectorisation that is preformed via numpy (in VectorizedTradingEnvironment) and the amount of
     multiprocessing processes.
-
-    This .... [creates a multiprocess vectorized wrapper for multiple environments, distributing each environment to its
-    own process, allowing significant speed up when the environment is computationally complex.
-
-    For performance reasons, if your environment is not IO bound, the number of environments should not exceed the
-    number of logical cores on your CPU.
-
-    .. warning::
-
-        Only 'forkserver' and 'spawn' start methods are thread-safe,
-        which is important when TensorFlow sessions or other non thread-safe
-        libraries are used in the parent (see issue #217). However, compared to
-        'fork' they incur a small start-up cost and have restrictions on
-        global variables. With those methods, users must wrap the code in an
-        ``if __name__ == "__main__":`` block.
-        For more information, see the multiprocessing documentation.
-
-    :param env_fns: Environments to run in subprocesses
-    :param start_method: method used to start the subprocesses.
-           Must be one of the methods returned by multiprocessing.get_all_start_methods().
-           Defaults to 'forkserver' on available platforms, and 'spawn' otherwise.]
     """
 
     def __init__(self, env_fns: List[Callable[[], gym.Env]], start_method: Optional[str] = None):
-        self.waiting = False
-        self.closed = False
-        n_envs = len(env_fns)
-
-        if start_method is None:
-            # Fork is not a thread safe method (see issue #217)
-            # but is more user friendly (does not require to wrap the code in
-            # a `if __name__ == "__main__":`)
-            forkserver_available = "forkserver" in mp.get_all_start_methods()
-            start_method = "forkserver" if forkserver_available else "spawn"
-        print(f"Start method is {start_method}")
-        ctx = mp.get_context(start_method)
-
-        self.remotes, self.work_remotes = zip(*[ctx.Pipe() for _ in range(n_envs)])
-        self.processes = []
-        for work_remote, remote, env_fn in zip(self.work_remotes, self.remotes, env_fns):
-            args = (work_remote, remote, CloudpickleWrapper(env_fn))
-            # daemon=True: if the main process crashes, we should not cause things to hang
-            process = ctx.Process(target=_worker, args=args, daemon=True)  # pytype:disable=attribute-error
-            process.start()
-            self.processes.append(process)
-            work_remote.close()
-
-        self.remotes[0].send(("get_spaces", None))
-        observation_space, action_space = self.remotes[0].recv()
+        super().__init__(self, env_fns, start_method)
 
         self.remotes[0].send(("get_attr", "num_trajectories"))
         num_trajectories_per_env = self.remotes[0].recv()
@@ -135,8 +92,7 @@ class MultiprocessTradingEnv(VecEnv):
         self.num_multiprocess_envs = len(self.remotes)
         self.n_steps = n_steps
         self.num_trajectories = len(env_fns) * num_trajectories_per_env
-
-        VecEnv.__init__(self, self.num_trajectories, observation_space, action_space)
+        self.num_envs = self.num_trajectories
 
     def step_async(self, actions: np.ndarray) -> None:
         multi_actions = self.flatten_multi(actions, inverse=True)
@@ -159,115 +115,9 @@ class MultiprocessTradingEnv(VecEnv):
         else:
             return array.reshape(self.num_multiprocess_envs * self.num_trajectories_per_env, -1).squeeze()
 
-    def seed(self, seed: Optional[int] = None) -> List[Union[None, int]]:
-        if seed is None:
-            seed = np.random.randint(0, 2**32 - 1)
-        for idx, remote in enumerate(self.remotes):
-            remote.send(("seed", seed + idx))
-        return [remote.recv() for remote in self.remotes]
-
     def reset(self) -> VecEnvObs:
         for remote in self.remotes:
             remote.send(("reset", None))
         obs = [remote.recv() for remote in self.remotes]
         obs = _flatten_obs(obs, self.observation_space)
         return self.flatten_multi(obs)
-
-    def close(self) -> None:
-        if self.closed:
-            return
-        if self.waiting:
-            for remote in self.remotes:
-                remote.recv()
-        for remote in self.remotes:
-            remote.send(("close", None))
-        for process in self.processes:
-            process.join()
-        self.closed = True
-
-    def get_images(self) -> Sequence[np.ndarray]:
-        for pipe in self.remotes:
-            # gather images from subprocesses
-            # `mode` will be taken into account later
-            pipe.send(("render", "rgb_array"))
-        imgs = [pipe.recv() for pipe in self.remotes]
-        return imgs
-
-    def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
-        """Return attribute from vectorized environment (see base class)."""
-        target_remotes = self._get_target_remotes(indices)
-        for remote in target_remotes:
-            remote.send(("get_attr", attr_name))
-        return [remote.recv() for remote in target_remotes]
-
-    def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
-        """Set attribute inside vectorized environments (see base class)."""
-        target_remotes = self._get_target_remotes(indices)
-        for remote in target_remotes:
-            remote.send(("set_attr", (attr_name, value)))
-        for remote in target_remotes:
-            remote.recv()
-
-    def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> List[Any]:
-        """Call instance methods of vectorized environments."""
-        target_remotes = self._get_target_remotes(indices)
-        for remote in target_remotes:
-            remote.send(("env_method", (method_name, method_args, method_kwargs)))
-        return [remote.recv() for remote in target_remotes]
-
-    def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None) -> List[bool]:
-        """Check if worker environments are wrapped with a given wrapper"""
-        target_remotes = self._get_target_remotes(indices)
-        for remote in target_remotes:
-            remote.send(("is_wrapped", wrapper_class))
-        return [remote.recv() for remote in target_remotes]
-
-    def _get_target_remotes(self, indices: VecEnvIndices) -> List[Any]:
-        """
-        Get the connection object needed to communicate with the wanted
-        envs that are in subprocesses.
-
-        :param indices: refers to indices of envs.
-        :return: Connection object to communicate between processes.
-        """
-        indices = self._get_indices(indices)
-        return [self.remotes[i] for i in indices]
-
-    def _get_indices(self, indices: VecEnvIndices) -> Iterable[int]:
-        """
-        Convert a flexibly-typed reference to environment indices to an implied list of indices.
-
-        :param indices: refers to indices of envs.
-        :return: the implied list of indices.
-        """
-        if indices is None:
-            indices = range(self.num_multiprocess_envs)
-        elif isinstance(indices, int):
-            indices = [indices]
-        return indices
-
-
-def _flatten_obs(obs: Union[List[VecEnvObs], Tuple[VecEnvObs]], space: spaces.Space) -> VecEnvObs:
-    """
-    Flatten observations, depending on the observation space.
-
-    :param obs: observations.
-                A list or tuple of observations, one per environment.
-                Each environment observation may be a NumPy array, or a dict or tuple of NumPy arrays.
-    :return: flattened observations.
-            A flattened NumPy array or an OrderedDict or tuple of flattened numpy arrays.
-            Each NumPy array has the environment index as its first axis.
-    """
-    assert isinstance(obs, (list, tuple)), "expected list or tuple of observations per environment"
-    assert len(obs) > 0, "need observations from at least one environment"
-
-    if isinstance(space, spaces.Dict):
-        assert isinstance(space.spaces, OrderedDict), "Dict space must have ordered subspaces"
-        assert isinstance(obs[0], dict), "non-dict observation for environment with Dict observation space"
-        return OrderedDict([(k, np.stack([o[k] for o in obs])) for k in space.spaces.keys()])
-    elif isinstance(space, spaces.Tuple):
-        assert isinstance(obs[0], tuple), "non-tuple observation for environment with Tuple observation space"
-        obs_len = len(space.spaces)
-        return tuple(np.stack([o[i] for o in obs]) for i in range(obs_len))
-    else:
-        return np.stack(obs)

f446d2640d3bf21d75f33326c00ea36ef1bd4502
Leandro Sánchez-Betancourt <leandro.sbetancourt@gmail.com>
2023-03-16 15:39:07 +0000

    add ASSET_PRICE_INDEX

--- mbt_gym/gym/helpers/plotting.py
+++ mbt_gym/gym/helpers/plotting.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 from mbt_gym.agents.Agent import Agent
 from mbt_gym.gym.TradingEnvironment import TradingEnvironment 
-from mbt_gym.gym.index_names import CASH_INDEX, INVENTORY_INDEX
+from mbt_gym.gym.index_names import CASH_INDEX, INVENTORY_INDEX, ASSET_PRICE_INDEX
 from mbt_gym.gym.helpers.generate_trajectory import generate_trajectory
 
 
